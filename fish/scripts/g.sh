d1ba5f206c27165c7d19cd2e4e9e81c6e2721022
Kirill Nagaitsev <knagaitsev@u.northwestern.edu>
2024-01-17 16:22:31 -0600

    fix calling of tools/gic.sh if it exists (wrap arg with quotes)

--- fish/scripts/g.sh
+++ fish/scripts/g.sh
@@ -8,7 +8,7 @@ fi
 
 if [ -e "./tools/gic.sh" ]; then
     echo "Running gic.sh"
-    ./tools/gic.sh $1
+    ./tools/gic.sh "$1"
 else
     git add -u && git commit -m "$1" && git push && git status
 fi
